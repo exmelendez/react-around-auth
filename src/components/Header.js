93a0b3385f21dc8dc5bee6aa8908aac2c8ad8a97
Eddie Melendez <emelendez@nycleadershipacademy.org>
2021-10-05 16:57:12 -0400

    implemented logout btn + function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,22 @@
 import { useContext } from 'react';
 import { CurrentUserContext } from '../contexts/CurrentUserContext';
+import { useHistory } from "react-router-dom";
 import logo from '../images/logo.svg';
 
 function Header() {
-  const { currentUser } = useContext(CurrentUserContext);
+  const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
+  const history = useHistory();
+  // console.log(history);
+
+  function handleLogout() {
+    console.log('logout btn clicked');
+    localStorage.removeItem('jwt');
+    setCurrentUser(prev => ({
+      ...prev,
+      isLoggedIn: false
+    }));
+    history.replace('/signin');
+  }
 
   return (
     <header className="header">
@@ -13,7 +26,7 @@ function Header() {
       <div className="header__content">
         <p className="header__text-container">
           { currentUser.isLoggedIn && (<span className="header__user-email">{currentUser.email}</span>) }
-          <a className="header__text-link" href="http://www.yahoo.com">
+          <a className="header__text-link" onClick={handleLogout}>
             <span className="header__text">{ currentUser.isLoggedIn ? 'Log out' : 'Log in'}</span>
           </a>
         </p>
