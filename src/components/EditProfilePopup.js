3f57b165d74501720ddf2abdcf15f8aa65f790f3
exmelendez <exmelendez@gmail.com>
2021-11-08 10:36:15 -0500

    completed mod of EditProfilePopup comp to use state var for inputs from App comp

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -6,7 +6,6 @@ function EditProfilePopup({handleInputChange, handleSubmit, isOpen, onClose, inp
   const { currentUser } = useContext(CurrentUserContext);
 
   useEffect(() => {
-    console.log('useEffect Ran');
     inputUpdate(prevState => ({
       ...prevState,
       name: currentUser.name,
@@ -16,17 +15,15 @@ function EditProfilePopup({handleInputChange, handleSubmit, isOpen, onClose, inp
   }, [isOpen, inputUpdate, currentUser.about, currentUser.name]);
 
   return (
-    <>
-      <PopupWithForm name={"edit-profile"} title={"Edit profile"} isOpen={isOpen} onClose={onClose} handleSubmit={handleSubmit}>
-        <input id="profile-name" onChange={handleInputChange} className="form__input form__input-profile-name" type="text" name="name" placeholder="Name"
-              minLength="2" maxLength="40" value={inputVals.name} required />
-        <span id="profile-name-error" className="form__error"></span>
+    <PopupWithForm name={"edit-profile"} title={"Edit profile"} isOpen={isOpen} onClose={onClose} handleSubmit={handleSubmit}>
+      <input id="profile-name" onChange={handleInputChange} className="form__input form__input-profile-name" type="text" name="name" placeholder="Name"
+            minLength="2" maxLength="40" value={inputVals.name} required />
+      <span id="profile-name-error" className="form__error"></span>
 
-        <input id="profile-title" onChange={handleInputChange} className="form__input form__input-profile-title" type="text" name="about"
-          placeholder="Title" minLength="2" maxLength="200" value={inputVals.about} required />
-        <span id="profile-title-error" className="form__error"></span>
-      </PopupWithForm>
-    </>
+      <input id="profile-title" onChange={handleInputChange} className="form__input form__input-profile-title" type="text" name="about"
+        placeholder="Title" minLength="2" maxLength="200" value={inputVals.about} required />
+      <span id="profile-title-error" className="form__error"></span>
+    </PopupWithForm>
   );
 }
 
