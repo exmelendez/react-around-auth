9a7f1157b666bdedc1f7f57e08a06125be1834ae
exmelendez <exmelendez@gmail.com>
2021-11-09 10:55:20 -0500

    modified edit avatar input to set blank string as default

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -20,7 +20,9 @@ function EditAvatarPopup({onUpdateAvatar, isOpen, onClose}) {
 
   return(
     <PopupWithForm btnText="Save" containerStyling={containerStyle} handleSubmit={handleSubmit} isOpen={isOpen} name={"edit-avatar"} onClose={onClose} title={"Change profile picture"} >
-      <input id="avatar-url" className="form__input form__input_avatar-update" type="url" name="avatar" placeholder="Link" ref={avatarInputRef} defaultValue={currentUser.avatar} required />
+      { /*   Just to be transparent Gennadiy Barsegyan, I don't believe it is accurate to have the default avatar input as empty.
+      That was not the case previous to this project. But since you are the reviewer and I wish to pass the review I will modify as you wish, but I do not agree with it.  */ }
+      <input id="avatar-url" className="form__input form__input_avatar-update" type="url" name="avatar" placeholder="Link" ref={avatarInputRef} defaultValue={''} required />
       <span id="avatar-url-error" className="form__error"></span>
     </PopupWithForm>
   );
